0e933224f89c52552a6b5b073a67a93b42a80a48
Elise Schell Bertol <elise.sb2007@aluno.ifsc.edu.br>
2026-03-26 21:38:34 +0000

    adicionar botao de nitro, trocar musica, adicionar comando de iniciar cena, iniciar codigo de quantidade de combustivel

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-import config from "./config.js";
+/*import config from "./config.js";
 import sceneMenu from "./sceneMenu.js";
 import scene1 from "./scene0.js";
 import scene0 from "./scene1.js";
@@ -15,5 +15,21 @@ class Game extends Phaser.Game {
 }
 
 window.onload = () => {
-  window.game = new Game();   
+  window.game = new Game();
+};*/
+
+import config from "./config.js";
+import scene0 from "./scene0.js";
+
+class Game extends Phaser.Game {
+  constructor() {
+    super(config);
+
+    this.scene.add("scene0", scene0);
+    this.scene.start("scene0");
+  }
 }
+
+window.onload = () => {
+  window.game = new Game();
+};
--- scene0.js
+++ scene0.js
@@ -5,6 +5,7 @@ class scene0 extends Phaser.Scene {
     this.threshold = 0.1;
     this.speed = 100;
     this.direction = undefined;
+    this.fuel = 100;
   }
 
   preload() {
@@ -22,79 +23,71 @@ class scene0 extends Phaser.Scene {
 
     this.load.image("asteroideum", "asteroideum.png");
 
-    this.load.audio("menusong", "menusong.mp3");
+    this.load.image("startf1", "startf1.png");
 
-    this.load.spritesheet("combustivel", "combustive.png", {
-      frameWidth: 128,
-      frameHeight: 128,
+    this.load.audio("songf1", "songf1.mp3");
+
+    this.load.spritesheet("combustivel", "combustivel.png", {
+      frameWidth: 64,
+      frameHeight: 64,
     });
 
     this.load.spritesheet("uispritesheet", "uispritesheet.png", {
       frameWidth: 800,
       frameHeight: 450,
     });
+
+    this.load.spritesheet("button", "button.png", {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
   }
 
   create() {
-    //this.add.image(400, 225, "mapf1");
-
+    this.physics.pause(); // Pausa a física para congelar a cena
+    
+    
+    this.input.once("pointerdown", () => {
+      this.physics.resume();
+      this.startf1.destroy(); // Remove a imagem de início
+    });
+    
     this.player = this.star = this.physics.add.image(800, 450, "star", 0); //SURGE NO MEIO DO MAPA
     this.star.setScale(3);
     this.player.setCollideWorldBounds(true);
-
-    this.music = this.sound.add("menusong");
+    
+    this.music = this.sound.add("songf1", { loop: true });
     this.music.play();
-
+    
     //this.player = this.physics.add.image(400, 225, "star", 0);
     //this.star = this.physics.add.image(400, 225, "star", 0);
     //this.cameras.main.setBounds(0, 0, 400, 225);
-
+    
     this.cameras.main.startFollow(this.player);
-
+    
     this.cameras.main.setBounds(0, 0, 800 * 2, 450 * 2);
     this.physics.world.setBounds(400, 225, 800, 450);
-
+    
     this.add.image(0, 0, "mapf1").setOrigin(0);
     this.add.image(800, 0, "mapf1").setOrigin(0).setFlipX(true);
     this.add.image(0, 450, "mapf1").setOrigin(0).setFlipY(true);
     this.add
-      .image(800, 450, "mapf1")
-      .setOrigin(0)
-      .setFlipX(true)
-      .setFlipY(true);
-
-    this.joystick = this.plugins.get("rexvirtualjoystickplugin").add(this, {
-      x: 100,
-      y: 350,
-      radius: 50,
-      base: this.add.circle(0, 0, 50, 0x888888),
-      thumb: this.add.circle(0, 0, 25, 0xcccccc),
-    });
-
-    this.joystick.on("update", () => {
-      //JOYSTICK ESTÁ POR BAIXO DOS ASTEROIDES PARA CARREGAR A TEMPO
-      const angle = Phaser.Math.DegToRad(this.joystick.angle);
-      const force = this.joystick.force;
-
-      if (force > this.threshold) {
-        this.direction = new Phaser.Math.Vector2(
-          Math.cos(angle),
-          Math.sin(angle),
-        ).normalize();
-      }
-
-      if (this.joystick.force > 0) {
-        this.star.setVelocity(
-          this.direction.x * this.speed,
-          this.direction.y * this.speed,
-        );
-      } else {
-        this.star.setVelocity(0, 0);
-      }
-    });
-
+    .image(800, 450, "mapf1")
+    .setOrigin(0)
+    .setFlipX(true)
+    .setFlipY(true);
+    
+    this.startf1 = this.add.image(800, 450, "startf1");
     this.uispritesheet = this.physics.add.sprite(800, 450, "uispritesheet", 1);
-
+    
+    this.textFuel = this.add.text(16, 16, `Fuel: ${this.fuel}`, {
+      fontSize: "32px",
+      fill: "#ffffff",
+      //deixar fixo na tela
+    });
+      
+    
+    
     // Asteroides
     this.asteroids = this.physics.add.group();
     const astPos = [
@@ -102,59 +95,108 @@ class scene0 extends Phaser.Scene {
       { x: 1100, y: 300 },
       { x: 1000, y: 600 },
     ];
-
-    astPos.forEach((pos) => {
-      const a = this.asteroids.create(pos.x, pos.y, "asteroideum");
-      a.setScale(1.5);
-      a.setCollideWorldBounds(true);
-      a.setBounce(1, 1);
-      a.setVelocity(
-        Phaser.Math.Between(-120, 120),
-        Phaser.Math.Between(-120, 120),
-      );
-    });
-
-    // Colisão
-    this.physics.add.collider(
-      this.player,
-      this.asteroids,
-      this.hitAsteroid,
-      null,
-      this,
-    );
-
+        
+        astPos.forEach((pos) => {
+          const a = this.asteroids.create(pos.x, pos.y, "asteroideum");
+          a.setScale(1);
+          a.setCollideWorldBounds(true);
+          a.setBounce(1, 1);
+          a.setVelocity(
+            Phaser.Math.Between(-120, 120),
+            Phaser.Math.Between(-120, 120),
+          );
+        });
+        
+        // Colisão
+        this.physics.add.collider(
+          this.player,
+          this.asteroids,
+          this.hitAsteroid,
+          null,
+          this,
+        );
+        
+        
+        this.combustivel = this.physics.add.group();
+        const comPos = [
+          { x: 500, y: 600 },
+          { x: 1100, y: 600 },
+          { x: 800, y: 200 },
+        ];
+        
+        this.anims.create({
+          key: "combustivel_anim",
+          frames: this.anims.generateFrameNumbers("combustivel", { start: 0, end: 3 }),
+          frameRate: 5,
+          repeat: -1,
+          
+        });
+        
+        comPos.forEach((pos) => {
+          const c = this.combustivel.create(pos.x, pos.y, "combustivel");
+          c.setScale(1);
+          c.setCollideWorldBounds(true);
+          
+        });
+        
+        
+        // Colisão
+        this.physics.add.collider(
+          this.player,
+          this.combustivel,
+          this.hitCombustivel,
+          null,
+          this,
+          this.fuel += 20,
+        );
+        
+        this.button = this.add
+        .sprite(1000, 600, "button", 0)
+        .setScale(2)
+        .setInteractive()
+        .on("pointerdown", () => {
+          this.button.setFrame(1)
+          this.player.setVelocity(
+            this.direction.x * this.speed * 2,
+            this.direction.y * this.speed * 2,);
+          })
+          .on("pointerup", () => {
+            this.button.setFrame(0);
+            this.player.setVelocity(
+              this.direction.x * this.speed / 2,
+              this.direction.y * this.speed / 2,
+            );
+          });
+        
+        this.joystick = this.plugins.get("rexvirtualjoystickplugin").add(this, {
+          x: 100,
+          y: 350,
+          radius: 50,
+         base: this.add.circle(0, 0, 50, 0x888888),
+         thumb: this.add.circle(0, 0, 25, 0xcccccc),
+        });
     
-    this.combustivel = this.physics.add.group();
-    const comPos = [
-      { x: 500, y: 600 },
-      { x: 1100, y: 600 },
-      { x: 800, y: 200 },
-    ];
-
-    this.anims.create({
-      key: "combustivel_anim",
-      frames: this.anims.generateFrameNumbers("combustivel", { start: 0, end: 3 }),
-      frameRate: 5,
-      repeat: -1,
-    });
+        this.joystick.on("update", () => {
+          //JOYSTICK ESTÁ POR BAIXO DOS ASTEROIDES PARA CARREGAR A TEMPO
+          const angle = Phaser.Math.DegToRad(this.joystick.angle);
+          const force = this.joystick.force;
+          
+          if (force > this.threshold) {
+            this.direction = new Phaser.Math.Vector2(
+              Math.cos(angle),
+              Math.sin(angle),
+            ).normalize();
+          }
     
-    comPos.forEach((pos) => {
-      const c = this.combustivel.create(pos.x, pos.y, "combustivel");
-      c.setScale(1.5);
-      c.setCollideWorldBounds(true);
-      
-    });
-    
-
-     // Colisão
-     this.physics.add.collider(
-       this.player,
-       this.combustivel,
-       this.hitCombustivel,
-       null,
-       this,
-     );
-    
+          if (this.joystick.force > 0) {
+            this.star.setVelocity(
+              this.direction.x * this.speed,
+              this.direction.y * this.speed,
+            );
+          } else {
+            this.star.setVelocity(0, 0);
+          }
+        });
     
   } //chave do create
 
@@ -165,6 +207,10 @@ class scene0 extends Phaser.Scene {
     combustivel.disableBody(true, true); }
 
   update() {}
+
+
+
+
 }
 export default scene0;
 
