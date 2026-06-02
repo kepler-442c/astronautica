b53072d056b0e6a1ac0702949a651ee11836c792
Elise Schell Bertol <elise.sb2007@aluno.ifsc.edu.br>
2026-06-02 13:27:47 +0000

    teste3

--- client/js/finalfeliz.js
+++ client/js/finalfeliz.js
@@ -17,7 +17,7 @@ export default class finalFeliz extends Phaser.Scene {
             .post(
               "https://feira-de-jogos.dev.br/api/v2/credit",
               {
-                product: 42, // id do jogo cadastrado no banco de dados da Feira de Jogos
+                product: 59, // id do jogo cadastrado no banco de dados da Feira de Jogos
                 value: 250, // crédito em tijolinhos
               },
               {
